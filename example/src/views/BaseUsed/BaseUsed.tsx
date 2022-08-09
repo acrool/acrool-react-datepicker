80f4f5d39cc1244f74a28d790c07fd8f1c92d5ca
imagine10255 <imagine10255@gmail.com>
2022-08-09 12:06:18 +0800

    fix: fix rangeDatepicker style bg

--- example/src/views/BaseUsed/BaseUsed.tsx
+++ example/src/views/BaseUsed/BaseUsed.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Datepicker, Timepicker, DateTimepicker, RangeDatepicker, IRangeDateValue} from 'bear-react-datepicker';
+import dayjs from 'dayjs';
 
 
 
@@ -14,19 +15,19 @@ const BaseUsed = () => {
         <div>
             <input type="text" value={myDate} onChange={(event) => setMyTime(event.target.value)}/>
             <div className="d-flex flex-row my-2">
-                <Datepicker value={myDate} onChange={setMyDate} isVisibleSetToday locale="zh-CN" className="mr-3"/>
+                <Datepicker value={myDate} onChange={setMyDate} isVisibleSetToday locale="zh-CN" format="YYYY/MM/DD" className="mr-3"/>
                 <Datepicker value={myDate} onChange={setMyDate} isVisibleSetToday locale="zh-CN" isDark/>
             </div>
 
-            <input type="text" style={{width: 220}} value={`${myRangeDate.startDate}~${myRangeDate.endDate}`} onChange={(event) => {
+            <input type="text" style={{width: 220}} value={`${myRangeDate.startDate ?? ''}~${myRangeDate.endDate ?? ''}`} onChange={(event) => {
                 const dateStr = event.target.value;
                 const dateObj = dateStr.split('~');
                 setMyRangeDate({startDate: dateObj[0], endDate: dateObj[1]})
             }}/>
             <div className="d-flex flex-row my-2">
-                <RangeDatepicker value={myRangeDate} onChange={setMyRangeDate} isVisibleSetToday locale="zh-CN" className="mr-3"/>
+                <RangeDatepicker value={myRangeDate} onChange={setMyRangeDate} isVisibleSetToday locale="zh-CN" format="YYYY/MM/DD"  className="mr-3"/>
 
-                <RangeDatepicker value={myRangeDate} onChange={setMyRangeDate} isVisibleSetToday locale="zh-CN" isDark minDate="2022-07-22" maxDate="2022-07-28"/>
+                <RangeDatepicker value={myRangeDate} onChange={setMyRangeDate} isVisibleSetToday locale="zh-CN" isDark minDate={dayjs().subtract(7, 'day').format('YYYY-MM-DD')} maxDate={dayjs().add(7, 'day').format('YYYY-MM-DD')}/>
             </div>
 
             <input type="text" value={myTime} onChange={(event) => setMyTime(event.target.value)}/>
