04bbece57dbac38102695733ba4dbc001de101c0
imagine10255 <imagine10255@gmail.com>
2024-06-25 15:39:54 +0800

    modify READEME

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,20 +8,30 @@ import {GridThemeProvider} from '@acrool/react-grid';
 function App() {
 
 
+    const renderHeader = () => {
+
+        const repositoryUrl = 'https://github.com/acrool/acrool-react-datepicker';
+        const name = 'Acrool React Datepicker';
+
+        return <>
+            <a href={repositoryUrl} target="_blank" rel="noopener noreferrer">
+                <Github width={40} height={40}/>
+            </a>
+
+            <div className="banner-wrapper">
+                <img src="/logo.svg" alt={name}/>
+                <h1>{name}</h1>
+            </div>
+        </>;
+    };
+
+
     return (
         <GridThemeProvider>
             <div className="App">
-                <a href="https://github.com/acrool/acrool-react-datepicker" target="_blank" rel="noopener noreferrer">
-                    <Github width={40} height={40}/>
-                </a>
-
-                <div className="banner-wrapper">
-                    <img src="/logo.svg" alt="Acrool React Locale"/>
-                    <h1>Acrool React Datepicker</h1>
-                </div>
+                {renderHeader()}
 
                 <Example/>
-
             </div>
         </GridThemeProvider>
     );
