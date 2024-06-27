b530b4c76ded0892293d79f9786d8a2a0e872aed
imagine10255 <imagine10255@gmail.com>
2024-06-27 14:04:31 +0800

    modify example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,35 +1,14 @@
 import './App.css';
 import Example from './views/Example';
-import Github from './assets/github.svg?react';
 import {GridThemeProvider} from '@acrool/react-grid';
-
+import Banner from './components/Banner';
 
 
 function App() {
-
-
-    const renderHeader = () => {
-
-        const repositoryUrl = 'https://github.com/acrool/acrool-react-datepicker';
-        const name = 'Acrool React Datepicker';
-
-        return <>
-            <a href={repositoryUrl} target="_blank" rel="noopener noreferrer">
-                <Github width={40} height={40}/>
-            </a>
-
-            <div className="banner-wrapper">
-                <img src="/logo.svg" alt={name}/>
-                <h1>{name}</h1>
-            </div>
-        </>;
-    };
-
-
     return (
         <GridThemeProvider>
             <div className="App">
-                {renderHeader()}
+                <Banner/>
 
                 <Example/>
             </div>
