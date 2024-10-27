64b1ae6320c30cc6b7db51a0e7c6c721d4981133
imagine10255 <imagine10255@gmail.com>
2024-10-27 10:59:43 +0800

    add storybook theme control

--- example/.storybook/preview.tsx
+++ example/.storybook/preview.tsx
@@ -4,9 +4,14 @@ import '@acrool/react-datepicker/dist/index.css';
 import '@acrool/react-grid/dist/index.css';
 import {GridThemeProvider} from "@acrool/react-grid";
 
+import { themes } from '@storybook/theming';
 
 const preview: Preview = {
   parameters: {
+    darkMode: {
+        dark: { ...themes.dark, appPreviewBg: '#000' },
+        light: { ...themes.dark, appPreviewBg: '#fff' }
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
